be69c3af607b4a35d8f9e95bc576cb0ee1a7a2db
Patri <patri@malvidopazo.es>
2022-02-04 10:15:25 +0100

    add changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const movies = require('./data/movies.json');
+const users = require ('./data/users.json');
 
 // create and config server
 const server = express();
@@ -13,8 +14,9 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get('/movies', (req, res) => {
 
+server.get('/movies', (req, res) => {
+  //PRIMERA OPCION-------------------
   // const response = {
   //   success: true,
   //   movies: movies,
@@ -23,16 +25,31 @@ server.get('/movies', (req, res) => {
   //   (movie) => movie.gender === req.query.gender
   // );
   // res.json(filterGender);
-  console.log(req.query);
-  const genderFilterParam= movies.filter((movie)=> movie.gender === req.query.gender)
-  res.send({
-    success: true,
-    movies:genderFilterParam
-  });
+  //------------------------------------------------------------
+  //SEGUNDO OPCION: funciona pero no filtra todas---------------
+  // console.log(req.query);
+  // const genderFilterParam= movies.filter((movie)=> movie.gender === req.query.gender)
+  // res.send({
+  //   success: true,
+  //   movies:genderFilterParam
+  // });
 
+ const genderFilterParam =req.query.gender;
+ const filterdMovies= movies.filter((movie)=>{
+   if  (genderFilterParam === ''){
+     return movie;
+   }else {
+     return movie.gender === genderFilterParam;
+   }
+ });
  
- 
+ res.json({
+   sucess: true,
+   movies: filterdMovies
+ });
 });
+
+//Aqui ponemos los ficheros estáticos
 const staticServerPath= "./src/public";
 server.use(express.static(staticServerPath));
 
@@ -41,5 +58,24 @@ server.use(express.static(staticServerMoviesImage));
 
 
 server.post("/login", (req,res)=>{
-  const 
-})
+  console.log(req.body);
+  const emailLogin = req.body.email;
+  const passwordLogin = req.body.password;
+  const foundUser = users.find(
+    (user) =>
+      user.password === passwordLogin && user.email === emailLogin
+  );
+  if (foundUser) {
+    res.json({
+      success: true,
+      userId: foundUser.id,
+    });
+  } else {
+    res.json({
+      success: false,
+      errorMessage: 'Usuario no encontrado',
+    });
+  }
+});
+
+
